6c785ff82d420ff25392ed39b63bfaac2d88fe33
bohdanuhryn <bohdanuhryn@gmail.com>
2016-02-04 13:24:16 +0200

    init commit

---
+++ app/src/main/java/com/bohdanuhryn/food2fork/fragments/MainFragment.java
@@ -0,0 +1,9 @@
+package com.bohdanuhryn.food2fork.fragments;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by BohdanUhryn on 04.02.2016.
+ */
+public class MainFragment extends Fragment {
+}
