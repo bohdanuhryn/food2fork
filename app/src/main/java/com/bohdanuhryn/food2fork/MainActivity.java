28066c384e86e02fd6f967bf522d0b929a194229
bohdanuhryn <bohdanuhryn@gmail.com>
2016-02-09 03:20:20 +0200

    search and loadmore updates

--- app/src/main/java/com/bohdanuhryn/food2fork/MainActivity.java
+++ app/src/main/java/com/bohdanuhryn/food2fork/MainActivity.java
@@ -1,20 +1,32 @@
 package com.bohdanuhryn.food2fork;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.SearchView;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 import com.bohdanuhryn.food2fork.fragments.MainFragment;
+import com.bohdanuhryn.food2fork.utils.ViewUtils;
 
 public class MainActivity extends AppCompatActivity implements MainFragment.OnMainFragmentListener {
 
     public static final String TAG = "MainActivity";
 
     private MainFragment mainFragment;
+    private SearchView searchView;
+
+    private String prevSearchQuery;
 
     @Override
     public void onStartRecipeActivity(long id) {
@@ -33,17 +45,30 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnMa
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        MenuItem searchItem = menu.findItem(R.id.main_action_search);
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        setupSearchView();
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.main_action_search:
-                SearchView searchView = (SearchView) MenuItemCompat.getActionView(item);
-                searchView.getQuery();
+            case R.id.action_sort_top:
+                actionSortTop();
+                item.setChecked(true);
+                return true;
+            case R.id.action_sort_tranding:
+                actionSortTranding();
+                item.setChecked(true);
+                return true;
+            case R.id.action_view_list:
+                actionViewList();
+                item.setChecked(true);
+                return true;
+            case R.id.action_view_table:
+                actionViewTable();
+                item.setChecked(true);
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -64,6 +89,69 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnMa
                 .beginTransaction()
                 .replace(R.id.fragment_container, mainFragment, MainFragment.TAG)
                 .commit();
+    }
+
+    private void setupSearchView() {
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                searchRecipe(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                EditText et = ViewUtils.getEditText(MainActivity.this, searchView);
+                if (et.getText().toString().isEmpty() && searchView.isFocused()) {
+                    searchRecipe("");
+                }
+                return false;
+            }
+        });
+        searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                /*if (hasFocus) {
+                    ((SearchView) v).setQuery(prevSearchQuery, false);
+                } else {
+
+                }*/
+            }
+        });
+    }
+
+    private void searchRecipe(String query) {
+        //prevSearchQuery = query;
+        if (mainFragment != null) {
+            mainFragment.setSearchQuery(query);
+            mainFragment.startSearch();
+        }
+    }
+
+    private void actionSortTop() {
+        if (mainFragment != null) {
+            mainFragment.setSearchSortType("r");
+            mainFragment.startSearch();
+        }
+    }
+
+    private void actionSortTranding() {
+        if (mainFragment != null) {
+            mainFragment.setSearchSortType("t");
+            mainFragment.startSearch();
+        }
+    }
+
+    private void actionViewList() {
+        if (mainFragment != null) {
+            mainFragment.setViewList();
+        }
+    }
+
+    private void actionViewTable() {
+        if (mainFragment != null) {
+            mainFragment.setViewTable();
+        }
     }
 
 }
--- app/src/main/java/com/bohdanuhryn/food2fork/fragments/MainFragment.java
+++ app/src/main/java/com/bohdanuhryn/food2fork/fragments/MainFragment.java
@@ -1,24 +1,25 @@
 package com.bohdanuhryn.food2fork.fragments;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.LoaderManager;
-import android.support.v4.content.Loader;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ProgressBar;
 
 import com.bohdanuhryn.food2fork.R;
 import com.bohdanuhryn.food2fork.adapters.RecipesAdapter;
-import com.bohdanuhryn.food2fork.loaders.RecipesListLoader;
 import com.bohdanuhryn.food2fork.models.Recipe;
 import com.bohdanuhryn.food2fork.models.RecipeSearchParams;
 import com.bohdanuhryn.food2fork.models.RecipesList;
+import com.bohdanuhryn.food2fork.service.F2fManager;
+import com.bohdanuhryn.food2fork.utils.EndlessRecyclerOnScrollListener;
 
 import java.util.ArrayList;
 
@@ -28,19 +29,19 @@ import butterknife.ButterKnife;
 /**
  * Created by BohdanUhryn on 04.02.2016.
  */
-public class MainFragment extends Fragment
-        implements LoaderManager.LoaderCallbacks<RecipesList> {
+public class MainFragment extends Fragment {
 
     public static final String TAG = "MainFragment";
 
     private OnMainFragmentListener mainFragmentListener;
 
     private View rootView;
-    @Bind(R.id.recipes_not_found_view)
-    TextView recipesNotFound;
     @Bind(R.id.recipes_recycler_view)
     RecyclerView recipesRecyclerView;
+    @Bind(R.id.recipes_progress_bar)
+    ProgressBar recipesProgressBar;
 
+    private RecyclerView.LayoutManager recipesLayoutManager;
     private RecipesAdapter recipesAdapter;
 
     private RecipeSearchParams recipeSearchParams;
@@ -48,6 +49,7 @@ public class MainFragment extends Fragment
 
     public static MainFragment newInstance() {
         MainFragment fragment = new MainFragment();
+        fragment.recipeSearchParams = new RecipeSearchParams();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
@@ -55,6 +57,38 @@ public class MainFragment extends Fragment
 
     public MainFragment() {
 
+    }
+
+    public void startSearch() {
+        if (recipeSearchParams == null) {
+            recipeSearchParams = new RecipeSearchParams();
+        }
+        recipeSearchParams.page = 1;
+        loadRecipes();
+    }
+
+    public void setSearchQuery(String query) {
+        if (recipeSearchParams == null) {
+            recipeSearchParams = new RecipeSearchParams();
+        }
+        recipeSearchParams.query = query;
+    }
+
+    public void setSearchSortType(String sort) {
+        if (recipeSearchParams == null) {
+            recipeSearchParams = new RecipeSearchParams();
+        }
+        recipeSearchParams.sort = sort;
+    }
+
+    public void setViewList() {
+        recipesLayoutManager = new LinearLayoutManager(getActivity());
+        recipesRecyclerView.setLayoutManager(recipesLayoutManager);
+    }
+
+    public void setViewTable() {
+        recipesLayoutManager = new GridLayoutManager(getActivity(), 2);
+        recipesRecyclerView.setLayoutManager(recipesLayoutManager);
     }
 
     @Override
@@ -74,28 +108,6 @@ public class MainFragment extends Fragment
         mainFragmentListener = null;
     }
 
-    @Override
-    public Loader<RecipesList> onCreateLoader(int id, Bundle args) {
-        return new RecipesListLoader(getActivity(), recipeSearchParams);
-    }
-
-    @Override
-    public void onLoadFinished(Loader<RecipesList> loader, RecipesList data) {
-        if (data != null) {
-            recipesList = data.recipes;
-            recipesNotFound.setVisibility(View.INVISIBLE);
-            setupRecipesAdapter();
-        } else {
-            recipesNotFound.setVisibility(View.VISIBLE);
-        }
-    }
-
-    @Override
-    public void onLoaderReset(Loader<RecipesList> loader) {
-        recipesList = null;
-        setupRecipesAdapter();
-    }
-
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -113,6 +125,28 @@ public class MainFragment extends Fragment
     private void setupRecipesRecyclerView() {
         LinearLayoutManager recipesLayoutManager = new LinearLayoutManager(getActivity());
         recipesRecyclerView.setLayoutManager(recipesLayoutManager);
+        recipesRecyclerView.setOnScrollListener(new EndlessRecyclerOnScrollListener(recipesLayoutManager) {
+            @Override
+            public void onLoadMore(int current_page) {
+                if (recipesList != null && recipeSearchParams != null) {
+                    ++recipeSearchParams.page;
+                    AsyncTask<Void, Void, RecipesList> at = new AsyncTask<Void, Void, RecipesList>() {
+
+                        @Override
+                        protected RecipesList doInBackground(Void... params) {
+                            return F2fManager.getRecipesList(recipeSearchParams);
+                        }
+
+                        @Override
+                        protected void onPostExecute(RecipesList rl) {
+                            recipesList.addAll(rl.recipes);
+                            recipesAdapter.notifyDataSetChanged();
+                        }
+                    };
+                    at.execute();
+                }
+            }
+        });
         setupRecipesAdapter();
     }
 
@@ -130,7 +164,26 @@ public class MainFragment extends Fragment
     }
 
     private void loadRecipes() {
-        getLoaderManager().restartLoader(0, null, this).forceLoad();
+        AsyncTask<Void, Void, RecipesList> at = new AsyncTask<Void, Void, RecipesList>() {
+
+            @Override
+            protected RecipesList doInBackground(Void... params) {
+                return F2fManager.getRecipesList(recipeSearchParams);
+            }
+
+            @Override
+            protected void onPreExecute() {
+                recipesProgressBar.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            protected void onPostExecute(RecipesList data) {
+                recipesList = data.recipes;
+                setupRecipesAdapter();
+                recipesProgressBar.setVisibility(View.INVISIBLE);
+            }
+        };
+        at.execute();
     }
 
     public interface OnMainFragmentListener {
